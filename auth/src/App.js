27c49cfed689c0e7ec7b44396fc1abd3f688ebc5
Davidson Castro <davilopescastro@gmail.com>
2018-03-09 14:26:53 -0300

    Commiss

--- auth/src/App.js
+++ auth/src/App.js
@@ -7,6 +7,8 @@ import LoginForm from './components/LoginForm';
 
 class App extends Component {
 
+  state = { loggedIn: false };
+
   componentWillMount() {
     firebase.initializeApp({
       apiKey: 'AIzaSyCylkOok_tdbBzLKIa95w4K6CrT2KKyhDA',
@@ -15,6 +17,14 @@ class App extends Component {
       projectId: 'authentication-cfd03',
       storageBucket: 'authentication-cfd03.appspot.com',
       messagingSenderId: '840614834794'
+    });
+
+    firebase.auth().onAuthStateChanged((user) => {
+          if (user) {
+            this.setState({ loggedIn: true });
+          } else {
+            this.setState({ loggedIn: false });
+          }
     });
   }
 
